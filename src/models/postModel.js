68ceb654d21a4260bf15167f628c54c41e25dcc4
Isabella <isabella.tds1.senai@gmail.com>
2025-04-01 11:44:03 -0300

    feat: create getPostById postModel

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -20,7 +20,7 @@ const createPost = async (user_id, description) => {
 
 const updatePost = async (id, user_id, description) => {
     const result = await pool.query(
-        "UPDATE posts SET user_id = $1, description = $2 WHERE id = $3 RETURNING *",
+        `UPDATE posts SET user_id = $1, description = $2 WHERE id = $3 RETURNING *`,
         [user_id, description, id]
     );
     return result.rows[0];
@@ -36,4 +36,12 @@ const deletePost = async (id) => {
     return { message: "Post deletado com sucesso!" };
 };
 
-module.exports = { getPosts, getPostById, createPost, updatePost, deletePost };
+const getPostByUser = async (user_id) => {
+    const result = await pool.query(
+        `SELECT posts.*, users.name AS username FROM posts LEFT JOIN users ON posts.user_id = users.id WHERE user_id = $1`,
+        [user_id]
+    );
+    return result.rows[0];
+}
+
+module.exports = { getPosts, getPostById, createPost, updatePost, deletePost, getPostByUser };
